26f4652f29e11ae05a27f351420dc633657d90db
Joe.gitau <joseph.karanja@hotmail.com>
2020-07-28 15:44:08 +0300

    add tests for the name attribute

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule,
       ],
       declarations: [
         AppComponent
@@ -14,22 +20,31 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'advanced-reactive-forms'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('advanced-reactive-forms');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('advanced-reactive-forms app is running!');
   });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should contain h4 tag with title "Registration Form"', () => {
+    const h4Element = fixture.debugElement.query(By.css('h4'));
+    expect(h4Element.nativeElement.textContent).toBe('Registration Form');
+  });
+
+  it('should validate that "name" is REQUIRED & MINIMUM_LENGTH(3)', () => {
+    let name = component.name;
+    expect(name.valid).toBeFalsy();
+    expect(name.pristine).toBeTruthy();
+
+    expect(name.errors.required).toBeTruthy();
+
+    name.setValue('Jo');
+    expect(name.errors.minlength).toBeTruthy();
+  });
+
 });
+
